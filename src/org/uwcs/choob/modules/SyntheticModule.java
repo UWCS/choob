84dfa5c95865e5fb7614a2e4ab3de7e87f635edf
bucko <isreal-choob@bucko.me.uk>
2005-08-27 18:18:04 +0000

    Ooops, forgot to add this one from ages ago, it seems.

---
+++ src/org/uwcs/choob/modules/SyntheticModule.java
@@ -0,0 +1,37 @@
+/*
+ * IntervalModule.java
+ *
+ * Created on August 5, 2005, 11:25 PM
+ */
+
+package org.uwcs.choob.modules;
+
+import java.util.*;
+import org.uwcs.choob.support.*;
+import org.uwcs.choob.support.events.*;
+import org.uwcs.choob.*;
+
+/**
+ *
+ * @author  sadiq
+ */
+public class SyntheticModule
+{
+	Choob bot;
+
+	/** Creates a new instance of IntervalModule */
+	public SyntheticModule( Choob bot )
+	{
+		this.bot = bot;
+	}
+
+	public void doSyntheticMessage( Message mes )
+	{
+		if( System.getSecurityManager() != null )
+		{
+			System.getSecurityManager().checkPermission(new ChoobPermission("canCreateEvents"));
+		}
+
+		bot.onSyntheticMessage( mes );
+	}
+}
