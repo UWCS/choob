72a2220e032e030b6b8da4bdc9be8cf8bbc8fd5e
sadiq <sadiq@uwcs.co.uk>
2005-08-29 22:50:13 +0000

    Woops! Fixed location of bucko's script of doom.

--- src/compile.sh
+++ src/compile.sh
@@ -3,9 +3,9 @@
 # Icky compile script but it'll work. For now.
 
 # Check for updates of HorriblePerlScript!
-if [ src/HorriblePerlScript.pl -nt org/uwcs/choob/Choob.java ]; then
+if [ misc/HorriblePerlScript.pl -nt org/uwcs/choob/Choob.java ]; then
 	echo Rebuilding event system...
-	perl src/HorriblePerlScript.pl
+	perl misc/HorriblePerlScript.pl
 fi
 
 javac -classpath lib/bsh-2.0b4.jar:lib/mysql-connector-java-3.1.8-bin.jar:lib/pircbot.jar org/uwcs/choob/support/events/*.java org/uwcs/choob/*.java org/uwcs/choob/support/*.java org/uwcs/choob/modules/*.java org/uwcs/choob/plugins/*.java
