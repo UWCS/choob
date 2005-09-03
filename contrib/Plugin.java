617ed3d5f921433122b4e90063c03325c5c98cae
faux <git@goeswhere.com>
2005-09-03 18:45:54 +0000

    Remove surplus argument from Plugin.LoadPlugin

--- contrib/Plugin.java
+++ contrib/Plugin.java
@@ -2,29 +2,55 @@ import org.uwcs.choob.*;
 import org.uwcs.choob.modules.*;
 import org.uwcs.choob.support.*;
 import org.uwcs.choob.support.events.*;
+import java.util.regex.*;
 
 class Plugin
 {
 	public void commandLoadPlugin( Message con, Modules modules, IRCInterface irc )
 	{
-		List parms = modules.util.getParams( con );
+		List params = modules.util.getParams( con );
 
-		if( parms.size() < 3 )
+		String url="";
+		String classname="";
+
+		if (params.size() == 2)
 		{
-			irc.sendContextReply(con, "Incorrect syntax! Dyooooooh!");
-			return;
+			Pattern pa;
+			Matcher ma;
+			url=params.get(1);
+			pa = Pattern.compile("^.*\\/([^\\/]+)\\.java$");
+			ma = pa.matcher(url);
+			if (ma.matches())
+				classname=ma.group(1);
+			else
+				irc.sendContextReply(con, "Unable to parse url (" + url + ") -> classname, please specify.");
+		}
+		else
+		{
+			if( params.size() != 3 )
+			{
+				irc.sendContextReply(con, "Syntax: [classname] url");
+				return;
+			}
+			else
+			{
+				url=params.get(2);
+				classname=params.get(1);
+				if ( classname.indexOf("/") != -1 )
+				{
+					irc.sendContextReply(con, "Arguments the other way around, you spoon.");
+					return;
+				}
+
+			}
 		}
 
-		if ( parms.get(1).indexOf("/") != -1 )
-		{
-			irc.sendContextReply(con, "Arguments the other way around, you spoon.");
-			return;
-		}
 
-		irc.sendContextReply(con, "Loading plugin.. " + parms.get(1));
+		irc.sendContextReply(con, "Loading plugin.. " + classname);
+
 		try
 		{
-			modules.plugin.addPlugin(parms.get(2), parms.get(1));
+			modules.plugin.addPlugin(url, classname);
 			irc.sendContextReply(con, "Plugin parsed, compiled and loaded!");
 		}
 		catch (Exception e)
@@ -36,13 +62,13 @@ class Plugin
 
 	public void commandReloadPluginPermissions( Message mes, Modules modules, IRCInterface irc )
 	{
-		List parms = modules.util.getParams( mes );
-		if (parms.size() != 2)
+		List params = modules.util.getParams( mes );
+		if (params.size() != 2)
 		{
 			irc.sendContextReply(mes, "You must specify a plugin name and only that.");
 			return;
 		}
-		modules.plugin.reloadPluginPermissions((String) parms.get(1));
-		irc.sendContextReply(mes, "OK, permissions reloaded for " + parms.get(1) + ".");
+		modules.plugin.reloadPluginPermissions((String) params.get(1));
+		irc.sendContextReply(mes, "OK, permissions reloaded for " + params.get(1) + ".");
 	}
 }
