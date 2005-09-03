0bbfcfe9c29bc5d3cfba467347bc44c64de1d9f3
faux <git@goeswhere.com>
2005-09-03 22:09:32 +0000

    Working Tell, although horribly unoptimised.

--- contrib/Tell.java
+++ contrib/Tell.java
@@ -10,24 +10,23 @@ import java.util.regex.*;
 // Note: This send/watch couple will break if someone changes their primary nick between the send and the receive, assuming they change their base nick.. it could be done otherwise, but Faux can't think of a way that doesn't involve mass database rapeage on every line sent by irc.
 // This entire plugin could do with some caching.
 
+private class TellObject
+{
+	public int id;
+	public String date;
+	public String from;
+	public String message;
+	public String target;
+}
 
 class Tell
 {
-	public void create()
-	{
-		Connection dbConnection = modules.dbBroker.getConnection();
-		PreparedStatement Smt = dbConnection.prepareStatement("CREATE TABLE IF NOT EXISTS `Tells` ( `Sent` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL, `From` TINYTEXT NOT NULL, `Target` TINYTEXT NOT NULL, `Message` TINYTEXT NOT NULL );");
-		Smt.executeUpdate();
-	}
-
 	public void commandSend( Message con, Modules mods, IRCInterface irc )
 	{
 		int targets = 0;
-		Connection dbConnection = mods.dbBroker.getConnection();
-		PreparedStatement insertLine = dbConnection.prepareStatement("INSERT INTO `Tells` VALUES(NOW(),?,?,?);");
-
+		TellObject tO = new TellObject();
 		// Note: This is intentionally not translated to a primary nick.
-		insertLine.setString(1, con.getNick()); // 'From'.
+		tO.from=con.getNick();
 
 		Pattern pa = Pattern.compile("^[^ ]+? ([a-zA-Z0-9_,|-]+) (.*)$");
 		Matcher ma = pa.matcher(con.getMessage());
@@ -38,39 +37,42 @@ class Tell
 			return;
 		}
 
-		insertLine.setString(3, ma.group(2)); // 'Message'.
+		tO.message=ma.group(2); // 'Message'.
 
 		StringTokenizer tokens = new StringTokenizer(ma.group(1), ",");
+
+		tO.date=(new Date()).toString();
+
 		while( tokens.hasMoreTokens() )
 		{
 			// Note: This call to getBestPrimaryNick is not optimal, discussed above.
-			insertLine.setString(2, mods.nick.getBestPrimaryNick(tokens.nextToken())); // 'Target'.
-			insertLine.executeUpdate();
+			tO.target=mods.nick.getBestPrimaryNick(tokens.nextToken());
+			System.out.println("Going to save!");
+			mods.odb.save(tO);
 			targets++;
 		}
 
 		irc.sendContextMessage(con, "Okay, will tell upon next speaking. (Sent to " + targets + " " + (targets == 1 ? "person" : "people") + ").");
 	}
 
-	public String filterWatchRegex = "";
-
-	public void filterWatch( Message con, Modules modules, IRCInterface irc )
+	private void spew(String nick, Modules mods, IRCInterface irc)
 	{
-		Connection dbConnection = modules.dbBroker.getConnection();
-		PreparedStatement Smt = dbConnection.prepareStatement("SELECT * FROM `Tells` WHERE `Target` = ?;");
-		Smt.setString(1, modules.nick.getBestPrimaryNick(con.getNick));
+		List results = mods.odb.retrieve (TellObject.class, "target = '" + nick + "'");
 
-		ResultSet Results = Smt.executeQuery();
-
-		if ( Results.first() )
-			do
+		if (results.size()!=0)
+			for (int i=0; i < results.size(); i++ )
 			{
-				irc.sendPrivateMessage(con.getNick(), "At " + Results.getString("Date") + ", " + Results.getString("From") + " told me to tell you: " + + Results.getString("Message"));
+				irc.sendMessage(nick, "At " + results.get(i).date + ", " + results.get(i).from + " told me to tell you: " + results.get(i).message);
+				mods.odb.delete(results.get(i));
 			}
-			while ( Results.next() );
 
-		modules.dbBroker.freeConnection(dbConnection);
+
 	}
+
+	public void onMessage( ChannelMessage ev, Modules mod, IRCInterface irc ) { spew(ev.getNick(), mod, irc); }
+	public void onPrivateMessage( PrivateMessage ev, Modules mod, IRCInterface irc ) { spew(ev.getNick(), mod, irc); }
+	public void onJoin( ChannelJoin ev, Modules mod, IRCInterface irc ) { spew(ev.getNick(), mod, irc); }
+
 
 	/*
 		onJoin()
@@ -80,3 +82,4 @@ class Tell
 		}
 	*/
 }
+
