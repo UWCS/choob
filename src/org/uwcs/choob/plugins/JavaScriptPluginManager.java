6478749bddbbc61016627b29dfa64602c44ddb40
silver <silver@uwcs.co.uk>
2005-10-10 00:37:15 +0000

    Adding generics and API support to JS plugins.

--- src/org/uwcs/choob/plugins/JavaScriptPluginManager.java
+++ src/org/uwcs/choob/plugins/JavaScriptPluginManager.java
@@ -30,6 +30,10 @@ public class JavaScriptPluginManager extends ChoobPluginManager {
 	private final Modules mods;
 	private final IRCInterface irc;
 	
+	private final int CALL_UNKNOWN     = 0;
+	private final int CALL_WANT_TASK   = 1;
+	private final int CALL_WANT_RESULT = 2;
+	
 	public JavaScriptPluginManager(Modules mods, IRCInterface irc) {
 		System.out.println("JavaScriptPluginManager.<ctor>");
 		this.mods = mods;
@@ -124,6 +128,7 @@ public class JavaScriptPluginManager extends ChoobPluginManager {
 	
 	protected void destroyPlugin(String pluginName) throws ChoobException {
 		System.out.println("JavaScriptPluginManager.destroyPlugin");
+		// FIXME: Implement this! //
 	}
 	
 	public ChoobTask commandTask(String pluginName, String command, Message ev) {
@@ -137,17 +142,18 @@ public class JavaScriptPluginManager extends ChoobPluginManager {
 	
 	public ChoobTask intervalTask(String pluginName, Object param) {
 		System.out.println("JavaScriptPluginManager.intervalTask");
+		// FIXME: Implement this! //
 		return null;
 	}
 	
 	public List<ChoobTask> eventTasks(IRCEvent ev) {
 		System.out.println("JavaScriptPluginManager.eventTasks");
+		// FIXME: Implement this! //
 		List<ChoobTask> tasks = new LinkedList<ChoobTask>();
 		return tasks;
 	}
 	
 	public List<ChoobTask> filterTasks(Message ev) {
-		System.out.println("JavaScriptPluginManager.filterTasks");
 		List<ChoobTask> tasks = new LinkedList<ChoobTask>();
 		List<JavaScriptPluginMethod> methods = pluginMap.getFilter(ev.getMessage());
 		if (methods != null) {
@@ -158,39 +164,64 @@ public class JavaScriptPluginManager extends ChoobPluginManager {
 		return tasks;
 	}
 	
-	public Object doAPI(String pluginName, String APIName, Object... params) throws ChoobException {
-		System.out.println("JavaScriptPluginManager.doAPI");
-		return null;
-	}
-	
 	public Object doGeneric(String pluginName, String prefix, String genericName, Object... params) throws ChoobException {
-		System.out.println("JavaScriptPluginManager.doGeneric");
+		String fullName = pluginName + "." + prefix + ":" + genericName;
+		System.out.println("JavaScriptPluginManager.doGeneric(" + fullName + ")");
+		
+		JavaScriptPluginMethod method = pluginMap.getGeneric(fullName);
+		if (method != null) {
+			return callMethod(method, params, CALL_WANT_RESULT);
+		}
 		return null;
 	}
 	
-	private ChoobTask callCommand(final JavaScriptPluginMethod method, final Object param)
+	public Object doAPI(String pluginName, String APIName, final Object... params) throws ChoobException {
+		System.out.println("JavaScriptPluginManager.doAPI");
+		return doGeneric(pluginName, "api", APIName, params);
+	}
+	
+	private ChoobTask callCommand(JavaScriptPluginMethod method, Object param)
 	{
-		final JavaScriptPlugin plugin = method.getPlugin();
-		String pluginName = plugin.getName();
-		
-		// Parameters for call.
 		Object[] params = { param, mods, irc };
 		
-		final Object[] params2 = params;
-		return new ChoobTask(pluginName) {
-			public void run() {
-				Context cx = Context.enter();
-				try {
-					Scriptable scope = plugin.getScope();
-					Scriptable inst = plugin.getInstance();
-					Function function = method.getFunction();
-					
-					function.call(cx, scope, inst, params2);
-				} finally {
-					cx.exit();
+		return (ChoobTask)callMethod(method, params, CALL_WANT_TASK);
+	}
+	
+	private Object callMethod(final JavaScriptPluginMethod method, final Object[] params, int result)
+	{
+		final JavaScriptPlugin plugin = method.getPlugin();
+		
+		if (result == CALL_WANT_TASK) {
+			String pluginName = plugin.getName();
+			final Object[] params2 = params;
+			return new ChoobTask(pluginName) {
+				public void run() {
+					Context cx = Context.enter();
+					try {
+						Scriptable scope = plugin.getScope();
+						Scriptable inst = plugin.getInstance();
+						Function function = method.getFunction();
+						
+						function.call(cx, scope, inst, params2);
+					} finally {
+						cx.exit();
+					}
 				}
+			};
+		}
+		if (result == CALL_WANT_RESULT) {
+			Context cx = Context.enter();
+			try {
+				Scriptable scope = plugin.getScope();
+				Scriptable inst = plugin.getInstance();
+				Function function = method.getFunction();
+				
+				return function.call(cx, scope, inst, params);
+			} finally {
+				cx.exit();
 			}
-		};
+		}
+		return null;
 	}
 }
 
@@ -202,18 +233,24 @@ final class JavaScriptPluginMap {
 	private final Map<String,List<String>> pluginCommands;
 	// List of filters for each plugin.
 	private final Map<String,List<NativeRegExp>> pluginFilters;
+	// List of generics for each plugin.
+	private final Map<String,List<String>> pluginGenerics;
 	
 	// List of function for each command.
 	private final Map<String,JavaScriptPluginMethod> commands;
 	// List of function for each filter.
 	private final Map<NativeRegExp,JavaScriptPluginMethod> filters;
+	// List of function for each generic.
+	private final Map<String,JavaScriptPluginMethod> generics;
 	
 	public JavaScriptPluginMap() {
 		plugins = new HashMap<String,Object>();
 		pluginCommands = new HashMap<String,List<String>>();
 		pluginFilters  = new HashMap<String,List<NativeRegExp>>();
+		pluginGenerics = new HashMap<String,List<String>>();
 		commands = new HashMap<String,JavaScriptPluginMethod>();
 		filters  = new HashMap<NativeRegExp,JavaScriptPluginMethod>();
+		generics = new HashMap<String,JavaScriptPluginMethod>();
 	}
 	
 	synchronized void loadPluginMap(String pluginName, JavaScriptPlugin pluginObj) {
@@ -226,6 +263,8 @@ final class JavaScriptPluginMap {
 		pluginCommands.put(lname, commandNames);
 		List<NativeRegExp> filterNames = new LinkedList<NativeRegExp>();
 		pluginFilters.put(lname, filterNames);
+		List<String> genericNames = new LinkedList<String>();
+		pluginGenerics.put(lname, genericNames);
 		
 		Scriptable inst = pluginObj.getInstance();
 		while (inst != null) {
@@ -283,7 +322,30 @@ final class JavaScriptPluginMap {
 						System.out.println("  Added filter: " + filterName);
 						
 					} else {
-						System.err.println("  Unknown property: " + propString);
+						Matcher matcher = Pattern.compile("([a-z]+)([A-Z].+)?").matcher(propString);
+						if (matcher.matches()) {
+							// Looks like a generic definition.
+							Object propVal = inst.get(propString, inst);
+							if (!(propVal instanceof Function)) {
+								System.err.println("  Generic-like property that is not a function: " + propString);
+								continue;
+							}
+							// It's a function, yay!
+							Function func = (Function)propVal;
+							JavaScriptPluginMethod method = new JavaScriptPluginMethod(pluginObj, propString, func);
+							
+							String prefix = matcher.group(1);
+							String gName = propString.substring(prefix.length()).toLowerCase();
+							String fullName = lname + "." + prefix + ":" + gName;
+							
+							genericNames.add(fullName);
+							generics.put(fullName, method);
+							
+							System.out.println("  Added generic: " + fullName);
+							
+						} else {
+							System.err.println("  Unknown property: " + propString);
+						}
 					}
 				} else {
 					System.out.println("  Found property of type: " + prop.getClass().getName());
@@ -338,6 +400,10 @@ final class JavaScriptPluginMap {
 			}
 		}
 		return rv;
+	}
+	
+	synchronized JavaScriptPluginMethod getGeneric(String genericName) {
+		return generics.get(genericName.toLowerCase());
 	}
 }
 
