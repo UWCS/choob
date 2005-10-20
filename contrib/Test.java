5e5cd9323808f1acc7ce95db2c972a7f44a6a03a
silver <silver@uwcs.co.uk>
2005-10-20 00:12:16 +0000

    *sigh*^2

--- contrib/Test.java
+++ contrib/Test.java
@@ -53,7 +53,7 @@ public class Test
 	{
 		List<String> params = mods.util.getParams( con );
 		if (params.size() > 0) {
-			System.exit(Integer(params.get(1)));
+			System.exit(new Integer(params.get(1)));
 		} else {
 			System.exit(1);
 		}
