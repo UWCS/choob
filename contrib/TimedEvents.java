c9adebad161fc93d868bb8042d0232a8c36ef900
faux <git@goeswhere.com>
2005-12-05 17:20:13 +0000

    TimedEvents' !at's leading 0 is now optional, and 12:MMpm is now correctly handled.

--- contrib/TimedEvents.java
+++ contrib/TimedEvents.java
@@ -134,7 +134,7 @@ public class TimedEvents
 
 		// Java--
 		GregorianCalendar cal = new GregorianCalendar();
-		Matcher ma = Pattern.compile("([0-9]|1[0-9]|2[0-3]):?([0-5][0-9])(?::?([0-5][0-9]))?(am|pm)?").matcher(time);
+		Matcher ma = Pattern.compile("((?:0?[0-9])|(?:1[0-9])|(?:2[0-3])):?([0-5][0-9])(?::?([0-5][0-9]))?(am|pm)?").matcher(time);
 		if (!ma.matches())
 		{
 			irc.sendContextReply(mes, "Bad time format: " + time + ".");
@@ -161,7 +161,7 @@ public class TimedEvents
 		{
 			if (ma.group(4).equalsIgnoreCase("pm"))
 			{
-				if (h<=12)
+				if (h<12)
 					h+=12;
 			}
 		}
