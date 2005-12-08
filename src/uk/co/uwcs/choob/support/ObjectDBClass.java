404a0ca280ef3f704af7eabbf61d8d9679a19c0c
silver <silver@uwcs.co.uk>
2005-12-08 23:22:50 +0000

    Added some JavaDoc comments, woo!

--- src/uk/co/uwcs/choob/support/ObjectDBClass.java
+++ src/uk/co/uwcs/choob/support/ObjectDBClass.java
@@ -3,6 +3,14 @@ package uk.co.uwcs.choob.support;
 import java.lang.reflect.*;
 
 public interface ObjectDBClass {
+	/**
+	 * Gets the name of the class, in an ObjectDB compatible format.
+	 * @returns The full class name, e.g. "plugin.MyPlugin.SomeClass".
+	 */
 	String getName();
+	
+	/**
+	 * Constructs a new instance of the represented class, and returns it.
+	 */
 	Object newInstance() throws InstantiationException, IllegalAccessException;
 }
--- src/uk/co/uwcs/choob/support/ObjectDBObject.java
+++ src/uk/co/uwcs/choob/support/ObjectDBObject.java
@@ -3,16 +3,42 @@ package uk.co.uwcs.choob.support;
 import java.lang.reflect.*;
 
 public interface ObjectDBObject {
-	// This is used to store the right class name in the DB and to validate loading objects.
+	/**
+	 * Gets the class name of the object in a ObjectDB-compatible format.
+	 * @returns The fully qualified class name (e.g. "plugin.MyPlugin.SomeClass").
+	 */
 	String getClassName();
 	
-	// The object must have an "ID" to be stored in the ObjectDB.
+	/**
+	 * Gets the unique ID value for the object. The ID identifies the object in the DB.
+	 */
 	int getId();
+	
+	/**
+	 * Sets the unique ID value for the object. The ID identifies the object in the DB.
+	 */
 	void setId(int id);
 	
-	// Gets and sets the information relating to the object's fields as stored in the ObjectDB.
+	/**
+	 * Gets all the fields that should be saved to the ObjectDB.
+	 * @returns An array of Strings containing the name of each property/field to save.
+	 */
 	String[] getFields();
+	
+	/**
+	 * Gets the type of a single property/field.
+	 * @returns Returns a Java TYPE class, either Boolean.TYPE, String.class, or some decendant of Number.
+	 */
 	Type getFieldType(String name) throws NoSuchFieldException;
+	
+	/**
+	 * Gets the value of a single property/field.
+	 * @returns The value contained in the property, as an Object. Can be typecast to whatever getFieldType returned.
+	 */
 	Object getFieldValue(String name) throws NoSuchFieldException, IllegalAccessException;
+	
+	/**
+	 * Sets the value of a single property/field. The type of the value should match the type returned by getFieldType.
+	 */
 	void setFieldValue(String name, Object value) throws NoSuchFieldException, IllegalAccessException;
 }
