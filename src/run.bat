a38fca82a8f23fb709549c60aa91cd2998fc1143
faux <git@goeswhere.com>
2005-12-14 12:18:04 +0000

    Updated mysql-connector-java to 3.1.12, and tested with mysql 5.
    Appears to fix #125, awaiting testing on another machine.

--- src/run.bat
+++ src/run.bat
@@ -5,7 +5,7 @@ REM # PLEASE UPDATE run.sh when you change this file (at least add a "# FIXME")
 REM #####
 
 :top
-java -cp .;lib/c3p0-0.9.0.2.jar;lib/msnm.jar;lib/jcfd.jar;lib/jazzy-core.jar;lib/bsh-2.0b4.jar;lib/mysql-connector-java-3.1.10-bin.jar;lib/pircbot.jar;lib/js-rhino-1.6r2.jar uk.co.uwcs.choob.ChoobMain
+java -cp .;lib/c3p0-0.9.0.2.jar;lib/msnm.jar;lib/jcfd.jar;lib/jazzy-core.jar;lib/bsh-2.0b4.jar;lib/mysql-connector-java-3.1.12-bin.jar;lib/pircbot.jar;lib/js-rhino-1.6r2.jar uk.co.uwcs.choob.ChoobMain
 IF "%ERRORLEVEL%"=="0" GOTO :EOF
 IF "%1"=="once" GOTO :EOF
 SLEEP 15
