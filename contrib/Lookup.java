f4514b69f25c902a26db846becbafa24a961e515
faux <git@goeswhere.com>
2006-07-01 14:00:49 +0000

    Lookup now goes to grab a few select fields from http://www.showmyip.com/ in the case that it can't get a reverse lookup.

--- contrib/Lookup.java
+++ contrib/Lookup.java
@@ -8,6 +8,8 @@ import java.net.*;
 import java.util.Hashtable;
 import javax.naming.*;
 import javax.naming.directory.*;
+import java.text.*;
+import java.io.*;
 
 public class Lookup
 {
@@ -32,7 +34,7 @@ public class Lookup
 		this.mods = mods;
 	}
 
-	private static final Hashtable env = new Hashtable();
+	private static final Hashtable<String, String> env = new Hashtable<String, String>();
 	static
 	{
 		env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
@@ -43,7 +45,87 @@ public class Lookup
 		try
 		{
 			InetAddress ia = InetAddress.getByName(s);
-			return ia.getHostName();
+			String hostname = ia.getHostName();
+			if (!hostname.trim().equalsIgnoreCase(s.trim()))
+				return hostname;
+			else
+			{
+				URL url;
+				try
+				{
+					url = new URL("http://www.showmyip.com/xml/?ip=" + URLEncoder.encode(s, "UTF-8") + "&app=Choob");
+				}
+				catch (UnsupportedEncodingException e)
+				{
+					return "Unexpected exception generating url.";
+				}
+				catch (MalformedURLException e)
+				{
+					return "Error, malformed url generated.";
+				}
+
+				String showmyip;
+				try
+				{
+					// Cache for a day.
+					showmyip = mods.scrape.getContentsCached(url, 24*60*60*1000);
+				}
+				catch (IOException e)
+				{
+					return "No direct lookup avaliable. " + e;
+				}
+
+				final String denied = "provided to subscribers only";
+
+				// Not generated at all, no-sir-ee.
+				final Pattern pcountry	=Pattern.compile("<lookup_country>(.+)</lookup_country>");
+				final Pattern pcountry2	=Pattern.compile("<lookup_country2>(.+)</lookup_country2>");
+				final Pattern pisp		=Pattern.compile("<lookup_isp>(.+)</lookup_isp>");
+				final Pattern porg		=Pattern.compile("<lookup_org>(.+)</lookup_org>");
+				final Pattern porg2		=Pattern.compile("<lookup_org2>(.+)</lookup_org2>");
+				final Pattern pcity		=Pattern.compile("<lookup_city>(.+)</lookup_city>");
+				final Pattern pcity2	=Pattern.compile("<lookup_city2>(.+)</lookup_city2>");
+
+				final Matcher mcountry	=pcountry		.matcher(showmyip);
+				final Matcher mcountry2	=pcountry2		.matcher(showmyip);
+				final Matcher misp		=pisp			.matcher(showmyip);
+				final Matcher morg		=porg			.matcher(showmyip);
+				final Matcher morg2		=porg2			.matcher(showmyip);
+				final Matcher mcity		=pcity			.matcher(showmyip);
+				final Matcher mcity2	=pcity2			.matcher(showmyip);
+
+				String scountry		=null; if (	mcountry	.find()) scountry	=mcountry	.group(1);
+				String scountry2	=null; if (	mcountry2	.find()) scountry2	=mcountry2	.group(1);
+				String sisp			=null; if (	misp		.find()) sisp		=misp		.group(1);
+				String sorg			=null; if (	morg		.find()) sorg		=morg		.group(1);
+				String sorg2		=null; if (	morg2		.find()) sorg2		=morg2		.group(1);
+				String scity		=null; if (	mcity		.find()) scity		=mcity		.group(1);
+				String scity2		=null; if (	mcity2		.find()) scity2		=mcity2		.group(1);
+
+				if (	scountry	!= null && scountry		.equalsIgnoreCase(denied))	scountry	=null;
+				if (	scountry2	!= null && scountry2	.equalsIgnoreCase(denied))	scountry2	=null;
+				if (	sisp		!= null && sisp			.equalsIgnoreCase(denied))	sisp		=null;
+				if (	sorg		!= null && sorg			.equalsIgnoreCase(denied))	sorg		=null;
+				if (	sorg2		!= null && sorg2		.equalsIgnoreCase(denied))	sorg2		=null;
+				if (	scity		!= null && scity		.equalsIgnoreCase(denied))	scity		=null;
+				if (	scity2		!= null && scity2		.equalsIgnoreCase(denied))	scity2		=null;
+
+				if (sorg		!= null && sorg2		!= null && sorg2		.trim().equalsIgnoreCase(sorg		.trim())) sorg2		=null;
+				if (scity		!= null && scity2		!= null && scity2		.trim().equalsIgnoreCase(scity		.trim())) scity2	=null;
+				if (scountry 	!= null && scountry2	!= null && scountry2	.trim().equalsIgnoreCase(scountry	.trim())) scountry2	=null;
+
+				if (sorg == null		&& sorg2		!= null) { sorg		= sorg2;		sorg2		=null; }
+				if (scity == null		&& scity2		!= null) { scity	= scity2;		scity2		=null; }
+				if (scountry == null	&& scountry2	!= null) { scountry	= scountry2;	scountry2	=null; }
+
+				return "No direct reverse lookup avaliable. " +
+					(scountry	!= null ? "Country: "		+ scountry	+ (scountry2	!= null ? " (or possibly: " + scountry2	+ ")" : "") + ";" : "") + " " +
+					(sorg		!= null ? "Organisation: "	+ sorg		+ (sorg2		!= null ? " (or possibly: " + sorg2		+ ")" : "") + ";" : "") + " " +
+					(scity		!= null ? "City: "			+ scity		+ (scity2		!= null ? " (or possibly: " + scity2	+ ")" : "") + ";" : "") + " " +
+					(sisp		!= null ? "Isp: "			+ sisp		+ "." : "");
+
+
+			}
 		}
 		catch(UnknownHostException e)
 		{
@@ -62,7 +144,6 @@ public class Lookup
 		{
 			attr = (new InitialDirContext( env )).getAttributes( what, new String[] { where }).get( where );
 
-
 			if( attr == null )
 				return "None found...";
 			else
@@ -75,7 +156,7 @@ public class Lookup
 						rep.append(", ");
 				}
 
-				return what + " ["  +where + "] --> " + rep.toString() + ".";
+				return what + " [" + where + "] --> " + rep.toString() + ".";
 			}
 		}
 		catch ( NameNotFoundException e )
@@ -96,9 +177,9 @@ public class Lookup
 	};
 	public void commandLookupIn( Message mes )
 	{
-
 		List<String> params = mods.util.getParams(mes, 2);
 		String domain, record;
+
 		if (params.size() <= 1)
 		{
 			irc.sendContextReply(mes, "Usage: <domain> [<record>].");
