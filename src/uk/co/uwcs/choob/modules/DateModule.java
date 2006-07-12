19920a9aa531ce000d6556b38765953b9713f1d5
faux <git@goeswhere.com>
2006-07-12 17:17:56 +0000

    Docs for DateModule.

--- src/uk/co/uwcs/choob/modules/DateModule.java
+++ src/uk/co/uwcs/choob/modules/DateModule.java
@@ -9,13 +9,16 @@ import uk.co.uwcs.choob.support.events.*;
 import java.util.*;
 import java.util.regex.*;
 
+/** Some functions to help with time and date manipulation */
 public final class DateModule
 {
+	/** enum representation of the levels of output that these functions produce */
 	public static enum longtokens { week, day, hour, minute, second, millisecond }
 
 	// Yes, this is incredibly lame. Enum -> String[] is really beyond me (without a for-loop):
 	public final static String[] longtokensstring = new String[] { "week", "day", "hour", "minute", "second", "millisecond" };
 
+	/** Helper, converts a long (ms since epoch) time to an array of weeks (index 0), days (index 1), hours, etc. */
 	final static long[] getSt(long i)
 	{
 		final long w= (i / (7*24*60*60*1000)); i -= w*(7*24*60*60*1000);
@@ -28,41 +31,54 @@ public final class DateModule
 		return st;
 	}
 
+	/** Gives a minimal representation of the given time interval, ie 1w6d. */
 	public final String timeMicroStamp(long i)
 	{
 		return timeMicroStamp(i, 2);
 	}
 
-	public final String timeMicroStamp(long i, int corse)
+	/** Gives a minimal representation of the given time interval, with the specified (maximum) number of elements. */
+	public final String timeMicroStamp(long i, int granularity)
 	{
-		return timeStamp(i, true, corse, longtokens.millisecond);
+		return timeStamp(i, true, granularity, longtokens.millisecond);
 	}
 
+	/** Gives a long representation of the given time interval, ie. "1 week and 6 days" */
 	public final String timeLongStamp(long i)
 	{
 		return timeLongStamp(i, 2);
 	}
 
-	public final String timeLongStamp(long i, int corse)
+	/** Gives a long representation of the given time interval, with the specified (maximum) number of elements. */
+	public final String timeLongStamp(long i, int granularity)
 	{
-		return timeStamp(i, false, corse, longtokens.millisecond);
+		return timeStamp(i, false, granularity, longtokens.millisecond);
 	}
 
-	public final String timeStamp(final long thing, final boolean shortTokens, int replyDetail, final longtokens minCorse)
+	/** General function for generating approximate string representations of time periods.
+	 * @param thing The time interval in question.
+	 * @param shortTokens Use the condensed form (1w6d) or generate full English (1 week and 6 days).
+	 * @param replyDetail Maximum number of parts to return.
+	 * @param minGranularity Minimum level of output, ie. passing 'days' here will cause 1w2d3h4m5s to only output 1w2d. Default is 'millisecond'.
+	 */
+	public final String timeStamp(final long thing, final boolean shortTokens, int replyDetail, final longtokens minGranularity)
 	{
 		final StringBuilder t = new StringBuilder();
 		final long st[]=getSt(thing);
 		final String tokenName[];
 
+		// Decide which tokens we're going to be using.
 		if (shortTokens)
 			tokenName = new String[] {"w","d","h","m","s","ms"};
 		else
 			tokenName = longtokensstring;
 
+		// Work out what we're going to output.
 		ArrayList<longtokens> useWhich = new ArrayList<longtokens>();
 
+		// Go through the "st", discard empty or invalid parts until we have enough (replyDetail).
 		for (int j = 0; j<st.length; j++)
-			if (j-1 == minCorse.ordinal())
+			if (j-1 == minGranularity.ordinal())
 				break;
 			else
 				if (st[j] != 0)
@@ -73,12 +89,14 @@ public final class DateModule
 
 		longtokens[] use = useWhich.toArray(new longtokens[] {});
 
+		// Special case, if we didn't decide to use anything, the period is less than the minGranuality, say so:
 		if (use.length == 0)
-			return t.append(shortTokens ? "<1" : "less than " + (minCorse == longtokens.hour ? "an " : "a "))
-				.append(tokenName[minCorse.ordinal()]).toString();
+			return t.append(shortTokens ? "<1" : "less than " + (minGranularity == longtokens.hour ? "an " : "a "))
+				.append(tokenName[minGranularity.ordinal()]).toString();
 		else
 			for (int i = 0; i<use.length; i++)
 			{
+				// Otherwise, go through each that we decided to use, and append it (and any padding required) to the return value.
 				final int j = use[i].ordinal();
 				if (shortTokens)
 					// 4h
@@ -90,7 +108,6 @@ public final class DateModule
 					.append(tokenName[j]).append(st[j]!=1 ? "s" : "")
 					.append(i==use.length-1 ? "" : (i!=use.length-2 ? ", " : " and "));
 			}
-
 		return t.toString();
 	}
 }
