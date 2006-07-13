709aa89a1cf6844c76eb6fcec057228bb15a62bb
bloodgod <choob@phenorbital.co.uk>
2006-07-13 10:26:52 +0000

    Added input filtering and support for product page redirection.

--- contrib/ReleaseDate.java
+++ contrib/ReleaseDate.java
@@ -53,6 +53,8 @@ public class ReleaseDate {
 	}
 	
 	//TODO: Return multiple results, and filter out "guide" results - as per the JB version this is re-implementing
+	//FIXME: Occaisionally gameplay add 'style="color: #aabbcc"' to some of the tags currently used for parsing.
+	//       These need taking into consideration as they are currently ignored by the regular expressions.
 	/**
 	 * The command provided by this plugin.
 	 * @param mes The command input from which parameters will be extracted.
@@ -60,22 +62,41 @@ public class ReleaseDate {
 	public void commandReleaseDate(Message mes) {
 		String param = mods.util.getParamString(mes);
 		try {
-			URL url = generateURL("http://shop.gameplay.co.uk/webstore/advanced_search.asp?keyword=", param);
-			Matcher noResultMatcher = getMatcher(url, "(?s)" + "Sorry, your search for" + "(.*?)" + "returned no results.");
-			if (noResultMatcher.find()) {
-				irc.sendContextReply(mes, "Sorry, no information was found for \"" + param + "\".");
-			} else {
-				Matcher gotResultMatcher = getMatcher(url, "(?s)" + "<h2 class=\"vlgWhite\">" + "(.*?)" + "<a href=\"productpage.asp?" + "(.*?)" +  "class=\"vlgWhite\">" + "(.*?)" + "</a></td></h2>" + "(.*?)" + "<div class=\"vsmorange10\">" + "(.*?)" + "</div>" + "(.*?)" + "<td valign=\"bottom\">" + "(.*?)" + "RRP");
-				if (gotResultMatcher.find()) {
-					irc.sendContextReply(mes, mods.scrape.readyForIrc(gotResultMatcher.group(3)).replaceAll("\\s+"," ") +  " (" +  mods.scrape.readyForIrc(gotResultMatcher.group(5)) + ")" + mods.scrape.readyForIrc(prettyReply(gotResultMatcher.group(7), url.toString(), 1)));
+			//Check for only sensible input with at least one alpha-numeric character.
+			Pattern dodgyCharPattern = Pattern.compile("^[\\s\\w\\-\\:\\;\\.\\,]*[a-zA-Z0-9]+[\\s\\w\\-\\:\\;\\.\\,]*$");
+			Matcher dodgyCharMatcher = dodgyCharPattern.matcher(param);
+			if (dodgyCharMatcher.matches()) {
+				URL url = generateURL("http://shop.gameplay.co.uk/webstore/advanced_search.asp?keyword=", param);
+				//TODO: Restructure code to be less "wow.. this was coded at 2am" (admittedly it was.. but it still sucks)
+				//Matcher for detecting when gameplay says there are no results
+				Matcher noResultMatcher = getMatcher(url, "(?s)" + "Sorry, your search for" + "(.*?)" + "returned no results.");
+				if (noResultMatcher.find()) {
+					irc.sendContextReply(mes, "Sorry, no information was found for \"" + param + "\".");
 				} else {
-					irc.sendContextReply(mes, "There was an error parsing the results. See " + url.toString());
+					//Matcher for detecting a search page full of results
+					Matcher gotResultMatcher = getMatcher(url, "(?s)" + "<h2 class=\"vlgWhite\">" + "(.*?)" + "<a href=\"productpage.asp?" + "(.*?)" +  "class=\"vlgWhite\">" + "(.*?)" + "</a></td></h2>" + "(.*?)" + "<div class=\"vsmorange10\">" + "(.*?)" + "</div>" + "(.*?)" + "<td valign=\"bottom\">" + "(.*?)" + "RRP");
+					if (gotResultMatcher.find()) {
+						irc.sendContextReply(mes, mods.scrape.readyForIrc(prettyReply(gotResultMatcher.group(3) + " (" + gotResultMatcher.group(5) + ")" + gotResultMatcher.group(7), url.toString(), 1)));
+					} else {
+						//Matcher for detecting content when we've been redirected to a product page
+						Matcher singlePageResultMatcher = getMatcher(url, "(?s)" + "<h1 class=\"bbHeader\">" + "(.*?)" + "</h1>" + "(.*?)" + "<td style=\"padding: 6px;\">" + "(.*?)" + "<img src=\"http://shop" + "(.*?)" + "<td valign=\"bottom\" colspan=\"2\">" + "(.*?)" + "<b>Category:</b><a href=");
+						if (singlePageResultMatcher.find()) {
+							irc.sendContextReply(mes, mods.scrape.readyForIrc(prettyReply(singlePageResultMatcher.group(1) + singlePageResultMatcher.group(5) + singlePageResultMatcher.group(3), url.toString(), 1)));
+						} else {
+							//It's clearly some unknown page type that isn't yet handled... Let the user deal with it!
+							irc.sendContextReply(mes, "There was an error parsing the results. See " + url.toString());
+						}
+					}
 				}
+			} else {
+				irc.sendContextReply(mes, "Sorry, I'm limited to A-Z0-9,._;: hyphen and space characters. At least one alpha-numeric character must be provided.");
 			}
 
 		} catch (LookupException e) {
 			irc.sendContextReply(mes, "Error looking up results.");
 		} catch (NullPointerException e) {
+			irc.sendContextReply(mes, "Error looking up results.");
+		} catch (IllegalStateException e) {
 			irc.sendContextReply(mes, "Error looking up results.");
 		}
 	}
@@ -85,7 +106,7 @@ public class ReleaseDate {
 		int maxlen=((irc.MAX_MESSAGE_LENGTH-15)*lines) - url.length();
 		text=text.replaceAll("\\s+"," ");
 		if (text.length()>maxlen) {
-			return text.substring(0, maxlen) + "..., see " + url + ".";
+			return text.substring(0, maxlen) + "..., see " + url;
 		} else {
 			return text + " See " + url + ".";
 		}
