503ccd6c28702bdd02718fb989e3cba4e4a19d9a
faux <git@goeswhere.com>
2006-07-24 09:21:43 +0000

    Pattern for See, don't ask.

--- contrib/alpha/See.java
+++ contrib/alpha/See.java
@@ -12,6 +12,9 @@ import java.text.DateFormatSymbols;
 public class See
 {
 
+	final SimpleDateFormat sdfa = new SimpleDateFormat("Kaa ");
+	final SimpleDateFormat sdfb = new SimpleDateFormat("EEEE");
+
 	private Modules mods;
 	private IRCInterface irc;
 	public See(IRCInterface irc, Modules mods)
@@ -25,6 +28,27 @@ public class See
 		return mods.date.timeStamp((new java.util.Date()).getTime()-d.getTime(), false, 3, uk.co.uwcs.choob.modules.DateModule.longtokens.minute);
 	}
 
+	private final synchronized ResultSet getDataFor(final String nick, final Connection conn) throws SQLException
+	{
+		final Statement stat=conn.createStatement();
+
+		stat.execute("DROP TEMPORARY TABLE IF EXISTS `tempt1`, `tempt2`; ");
+
+		{
+			final PreparedStatement s=conn.prepareStatement("CREATE TEMPORARY TABLE `tempt1` AS SELECT `Time` FROM `History` WHERE `Time` > " +  (System.currentTimeMillis()-(1000*60*60*24*5)) + " AND (CASE INSTR(`Nick`,'|') WHEN 0 THEN `Nick` ELSE LEFT(`Nick`, INSTR(`Nick`,'|')-1) END)=? AND `Channel`IS NOT NULL ORDER BY `Time`; ");
+			s.setString(1, nick);
+			s.executeUpdate();
+		}
+
+		stat.execute("ALTER TABLE `tempt1` ADD `index` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST; ");
+
+		stat.execute("CREATE TEMPORARY TABLE `tempt2` as SELECT * from `tempt1`; " );
+		stat.execute("UPDATE `tempt2` SET `index`:=`index`+1; " );
+		stat.execute("ALTER TABLE `tempt2` ADD PRIMARY KEY ( `index` ); ");
+
+		return conn.prepareStatement("SELECT DATE_ADD(FROM_UNIXTIME( `tempt1`.`Time` /1000 ), INTERVAL ((`tempt2`.`Time` - `tempt1`.`Time` ) /1000) SECOND) as `start`, FROM_UNIXTIME( `tempt1`.`Time` /1000 ) AS `end`, ((`tempt1`.`Time` - `tempt2`.`Time` ) /1000 /3600) AS `diff` FROM `tempt2` INNER JOIN `tempt1` ON `tempt2`.`index` = `tempt1`.`index` HAVING `diff` > 6;").executeQuery();
+	}
+
 	public final synchronized void commandBodyClock( Message mes ) throws SQLException
 	{
 		String nick=mods.util.getParamString(mes).trim();
@@ -36,27 +60,8 @@ public class See
 
 		final Connection conn=mods.odb.getConnection();
 
-		{
-			final Statement stat=conn.createStatement();
+		ResultSet rs = getDataFor(nick, conn);
 
-			stat.execute("DROP TEMPORARY TABLE IF EXISTS `tempt1`, `tempt2`; ");
-
-			{
-				final PreparedStatement s=conn.prepareStatement("CREATE TEMPORARY TABLE `tempt1` AS SELECT `Time` FROM `History` WHERE `Time` > " +  (System.currentTimeMillis()-(1000*60*60*24*5)) + " AND (CASE INSTR(`Nick`,'|') WHEN 0 THEN `Nick` ELSE LEFT(`Nick`, INSTR(`Nick`,'|')-1) END)=? AND `Channel`IS NOT NULL ORDER BY `Time`; ");
-				s.setString(1, nick);
-				s.executeUpdate();
-			}
-
-			stat.execute("ALTER TABLE `tempt1` ADD `index` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST; ");
-
-			stat.execute("CREATE TEMPORARY TABLE `tempt2` as SELECT * from `tempt1`; " );
-			stat.execute("UPDATE `tempt2` SET `index`:=`index`+1; " );
-			stat.execute("ALTER TABLE `tempt2` ADD PRIMARY KEY ( `index` ); ");
-		}
-
-		final PreparedStatement sel=conn.prepareStatement("SELECT DATE_ADD(FROM_UNIXTIME( `tempt1`.`Time` /1000 ), INTERVAL ((`tempt2`.`Time` - `tempt1`.`Time` ) /1000) SECOND) as `start`, FROM_UNIXTIME( `tempt1`.`Time` /1000 ) AS `end`, ((`tempt1`.`Time` - `tempt2`.`Time` ) /1000 /3600) AS `diff` FROM `tempt2` INNER JOIN `tempt1` ON `tempt2`.`index` = `tempt1`.`index` HAVING `diff` > 6;");
-
-		ResultSet rs = sel.executeQuery();
 		String ret="";
 
 		if (!rs.last())
@@ -85,5 +90,51 @@ public class See
 
 		mods.odb.freeConnection(conn);
 	}
+
+
+	private final String datelet(Date d)
+	{
+		return sdfa.format(d).toLowerCase() + sdfb.format(d);
+	}
+
+	public final synchronized void commandPattern( Message mes ) throws SQLException
+	{
+		String nick=mods.util.getParamString(mes).trim();
+
+		if (nick.equals(""))
+			nick=mes.getNick();
+
+		nick=mods.nick.getBestPrimaryNick(nick);
+
+		final Connection conn=mods.odb.getConnection();
+
+		ResultSet rs = getDataFor(nick, conn);
+
+		if (!rs.first())
+			irc.sendContextReply(mes, "I don't have enough information about " + nick + ".");
+		else
+		{
+			rs.beforeFirst();
+
+
+			String ret=nick + " was sleeping: ";
+			while (rs.next())
+			{
+				final Timestamp gotup=rs.getTimestamp("end");
+				final Date start = new Date(rs.getTimestamp("start").getTime());
+				final Date end = new Date(rs.getTimestamp("end").getTime());
+				ret += datelet(start) + " -> " + datelet(end) + ", ";
+				System.out.println(start.getTime() + " " + end.getTime());
+			}
+
+			if (ret.length()>2)
+				ret = ret.substring(0, ret.length() -2);
+			irc.sendContextReply(mes, ret + ".");
+		}
+
+
+		mods.odb.freeConnection(conn);
+	}
+
 
 }
