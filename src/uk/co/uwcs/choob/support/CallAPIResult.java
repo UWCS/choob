75643a7b297921fbc4b89fab5e10fbfdd8cdd839
silver <silver@uwcs.co.uk>
2006-08-16 13:35:35 +0000

    Added new file that SVN kindly forgot to tell me about.

---
+++ src/uk/co/uwcs/choob/support/CallAPIResult.java
@@ -0,0 +1,42 @@
+/*
+ * CallAPIResult.java
+ *
+ * Created on August 16, 2006, 1:06 AM
+ */
+package uk.co.uwcs.choob.support;
+
+public final class CallAPIResult
+{
+	private String pluginName;
+	private Object object;
+	private Exception exception;
+	
+	public CallAPIResult(String pluginName, Object object, Exception exception) {
+		this.pluginName = pluginName;
+		this.object     = object;
+		this.exception  = exception;
+	}
+	
+	public String getPluginName() {
+		return pluginName;
+	}
+	
+	public Object getObject() {
+		return object;
+	}
+	
+	public Exception getException() {
+		return exception;
+	}
+	
+	public Boolean isSuccessful() {
+		return (object != null) && (exception == null);
+	}
+	
+	public String toString() {
+		if (exception != null) {
+			return "{" + pluginName + "} " + exception.toString();
+		}
+		return "{" + pluginName + "} " + object.toString();
+	}
+}
