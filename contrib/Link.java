953e84930fa895c84af08ccef47e22aa5020f7dd
faux <git@goeswhere.com>
2006-08-28 21:55:42 +0000

    Go, Ctrl+Shift+F, GO!

--- contrib/Link.java
+++ contrib/Link.java
@@ -14,7 +14,8 @@ import java.io.*;
  *
  */
 
-public class OldLink {
+public class OldLink
+{
 	public int id;
 	public String URL;
 	public String poster;
@@ -23,35 +24,34 @@ public class OldLink {
 	public long lastPostedTime;
 }
 
-public class Link {
-	public String[] info() {
-		return new String[] {
+public class Link
+{
+	public String[] info()
 			"Plugin which matches on links.",
 			"Tim Retout /  Chris Hawley",
 			"tim@retout.co.uk /  choob@blood-god.co.uk",
 			"$Rev$$Date$"
 		};
 	}
-	
+
 	Modules mods;
+
 	IRCInterface irc;
-	
+
 	/**
-	 * Specifies the minimum time between when the bot last saw the link,
-	 * and when it starts complaining about it being ooooold.
+	 * Specifies the minimum time between when the bot last saw the link, and when it starts complaining about it being ooooold.
 	 */
 	private static final long FLOOD_INTERVAL = 15 * 60 * 1000; // 15 minutes
-	
-	public Link(Modules mods, IRCInterface irc) {
+
+	public Link(Modules mods, IRCInterface irc)
+	{
 		this.irc = irc;
 		this.mods = mods;
-		
-		//Purge
+
+		// Purge
 		/*
-		List<OldLink> links = mods.odb.retrieve(OldLink.class, "");
-		for (OldLink link : links) {
-			mods.odb.delete(link);
-		} */
+		 * List<OldLink> links = mods.odb.retrieve(OldLink.class, ""); for (OldLink link : links) { mods.odb.delete(link); }
+		 */
 	}
 
 	/** Exceptions to what can be "olded". */
@@ -64,18 +64,20 @@ public class Link {
 		+ "(?:[^/]*\\.)?(?:uwcs|warwickcompsoc)\\.co\\.uk"		// compsoc
 		+ ")"
 	);
-	
 	/** Regex to match links. */
 	public static String filterLinkRegex = "http://\\S*";
+
 	final private static Pattern linkPattern = Pattern.compile(filterLinkRegex);
-	
+
 	/**
 	 * Called for every line that contains a link.
+	 *
 	 * @param mes The message containing the link.
 	 * @param mods The bot modules.
 	 * @param irc The irc interface.
 	 */
-	public void filterLink(Message mes, Modules mods, IRCInterface irc) {
+	public void filterLink(Message mes, Modules mods, IRCInterface irc)
+	{
 		// Ignore stuff that isn't a channel message or action
 		// Ignore synthetic messages
 		// Ignore commands
@@ -88,13 +90,14 @@ public class Link {
 		ArrayList<OldLink> oldLinks = new ArrayList<OldLink>();
 
 		// Iterate over links in line.
-		while (linkMatch.find()) {
+		while (linkMatch.find())
+		{
 			String link = linkMatch.group(0);
 			OldLink linkObj = getOldLink(link);
-			if ( linkObj != null ) {
+			if (linkObj != null)
+			{
 				// Flood protection
-				if ( System.currentTimeMillis() - linkObj.lastPostedTime
-								< FLOOD_INTERVAL )
+				if (System.currentTimeMillis() - linkObj.lastPostedTime < FLOOD_INTERVAL)
 					continue;
 
 				// Old link: Update the last-posted time.
@@ -102,11 +105,13 @@ public class Link {
 				mods.odb.update(linkObj);
 
 				// Don't "old" reposts from the same user
-				if ( linkObj.poster.equals(mes.getNick()) )
+				if (linkObj.poster.equals(mes.getNick()))
 					continue;
 
-				oldLinks.add( linkObj );
-			} else {
+				oldLinks.add(linkObj);
+			}
+			else
+			{
 				// New link: Add to database.
 				linkObj = new OldLink();
 				linkObj.URL = link;
@@ -118,59 +123,60 @@ public class Link {
 			}
 		}
 
-		if ( oldLinks.size() == 0 ) {
+		if (oldLinks.size() == 0)
 			return;
-		} else if ( oldLinks.size() == 1 ) {
-			irc.sendContextReply( mes, getOldResponse(oldLinks.get(0)) );
-		} else {
+		else if (oldLinks.size() == 1)
+			irc.sendContextReply(mes, getOldResponse(oldLinks.get(0)));
+		else
 			irc.sendContextAction( mes, "slaps " + mes.getNick()
 				+ " for spamming " + oldLinks.size()
 				+ " ooooolllllddddd links." );
-		}
 	}
-	
+
 	/**
 	 * Command to manually check whether a link is old.
+	 *
 	 * @param mes The message delivering the command.
 	 */
-	public void commandIsOld(Message mes) {
+	public void commandIsOld(Message mes)
+	{
 		// Ignore messages in channels.
-		if ( mes instanceof ChannelMessage )
+		if (mes instanceof ChannelMessage)
 			return;
 
 		Matcher linkMatch = linkPattern.matcher(mes.getMessage());
 
-		if ( linkMatch.find() ) {
+		if (linkMatch.find())
+		{
 			String link = linkMatch.group(0);
 			OldLink linkObj = getOldLink(link);
-			if (linkObj == null) {
-				irc.sendContextReply( mes, "Link is not old." );
-			} else if ( System.currentTimeMillis() - linkObj.lastPostedTime
-								< FLOOD_INTERVAL ) {
-				irc.sendContextReply( mes, "Link is old, but posted recently.");
-			} else if ( linkObj.poster.equals(mes.getNick()) ) {
-				irc.sendContextReply( mes, "Link is old, but you posted it." );
-			} else {
-				irc.sendContextReply( mes, getOldResponse(linkObj) );
-			}
-		} else {
-			irc.sendContextReply( mes, "Usage: !isold <link>" );
+			if (linkObj == null)
+				irc.sendContextReply(mes, "Link is not old.");
+			else if (System.currentTimeMillis() - linkObj.lastPostedTime < FLOOD_INTERVAL)
+				irc.sendContextReply(mes, "Link is old, but posted recently.");
+			else if (linkObj.poster.equals(mes.getNick()))
+				irc.sendContextReply(mes, "Link is old, but you posted it.");
+			else
+				irc.sendContextReply(mes, getOldResponse(linkObj));
 		}
+		else
+			irc.sendContextReply(mes, "Usage: !isold <link>");
 	}
-	
+
 	/**
 	 * Check the database to see if a particular link is old.
+	 *
 	 * @param link The URI to check the database for.
 	 * @return The link object if present in the database, else null.
 	 */
-	private OldLink getOldLink(String link) {
+	private OldLink getOldLink(String link)
+	{
 		// Ensure that the link isn't in our exceptions list
 		if (exceptionPattern.matcher(link).find())
 			return null;
 
 		// Check objectdb
-		String queryString = "WHERE URL = \""
-								+ mods.odb.escapeString(link) + "\"";
+		String queryString = "WHERE URL = \"" + mods.odb.escapeString(link) + "\"";
 		List<OldLink> links = mods.odb.retrieve(OldLink.class, queryString);
 
 		// Return the first result, if any.
@@ -179,13 +185,14 @@ public class Link {
 
 	/**
 	 * Get the "ooold" response for an OldLink.
+	 *
 	 * @param linkObj The olde linke to fetche a response for.
 	 * @return A suitably harsh response.
 	 */
-	private String getOldResponse(OldLink linkObj) {
-		long timeSinceOriginal = 
-			System.currentTimeMillis() - linkObj.firstPostedTime;
-		int oldHours = (int) timeSinceOriginal / ( 60 * 60 * 1000 );
+	private String getOldResponse(OldLink linkObj)
+	{
+		long timeSinceOriginal = System.currentTimeMillis() - linkObj.firstPostedTime;
+		int oldHours = (int) timeSinceOriginal / (60 * 60 * 1000);
 
 		// Represent the number of hours since the original posting,
 		// in base 2, using upper and lower case 'o's.
@@ -198,54 +205,50 @@ public class Link {
 			+ " ago by " + linkObj.poster + ")";
 	}
 
-  public void webListLinks(PrintWriter out, String params, String[] user)
-  { 
-    out.println("HTTP/1.0 200 OK");
-    out.println("Content-Type: text/html");
-    out.println();
-      
-    int timePeriod;
-    
-    try
-    {
-      timePeriod = Integer.parseInt(params);
-    }
-    catch( NumberFormatException e )
-    {
-      out.println("<center><blink><h1>ERROR IN PARAMETER</h1></blink></center>");
-      return;
-    }
-    
-    long cutOff = System.currentTimeMillis() - (timePeriod * 60 * 60 * 1000);
-    
-    String queryString = "SORT DESC INTEGER lastPostedTime WHERE firstPostedTime > " + cutOff;
-    
-    List<OldLink> links = mods.odb.retrieve(OldLink.class, queryString);
-    
-    for( OldLink link : links )
-    {
-      out.println("<a href=\"" + link.URL + "\">" + link.URL + "</a>&nbsp;-&nbsp;" + link.poster + " in " + link.channel + "<br />");
-    }
-    
-    out.flush();
-  }
-  
-  public void webLastLink(PrintWriter out, String params, String[] user)
-  { 
-    out.println("HTTP/1.0 200 OK");
-    out.println("Content-Type: text/html");
-    out.println();
-      
-    String queryString = "SORT DESC INTEGER lastPostedTime LIMIT (1)";
-    
-    List<OldLink> links = mods.odb.retrieve(OldLink.class, queryString);
-    
-    for( OldLink link : links )
-    {
-      out.println("<HTML><HEAD><SCRIPT>window.location = \"" + link.URL + "\";</SCRIPT></HEAD><BODY></BODY></HTML>");    
-    }
-    
-    out.println();
-    out.flush();
-  }  
+	public void webListLinks(PrintWriter out, String params, String[] user)
+	{
+		out.println("HTTP/1.0 200 OK");
+		out.println("Content-Type: text/html");
+		out.println();
+
+		int timePeriod;
+
+		try
+		{
+			timePeriod = Integer.parseInt(params);
+		}
+		catch( NumberFormatException e )
+		{
+			out.println("<center><blink><h1>ERROR IN PARAMETER</h1></blink></center>");
+			return;
+		}
+
+		long cutOff = System.currentTimeMillis() - (timePeriod * 60 * 60 * 1000);
+
+		String queryString = "SORT DESC INTEGER lastPostedTime WHERE firstPostedTime > " + cutOff;
+
+		List<OldLink> links = mods.odb.retrieve(OldLink.class, queryString);
+
+		for( OldLink link : links )
+			out.println("<a href=\"" + link.URL + "\">" + link.URL + "</a>&nbsp;-&nbsp;" + link.poster + " in " + link.channel + "<br />");
+
+		out.flush();
+	}
+
+	public void webLastLink(PrintWriter out, String params, String[] user)
+	{
+		out.println("HTTP/1.0 200 OK");
+		out.println("Content-Type: text/html");
+		out.println();
+
+		String queryString = "SORT DESC INTEGER lastPostedTime LIMIT (1)";
+
+		List<OldLink> links = mods.odb.retrieve(OldLink.class, queryString);
+
+		for( OldLink link : links )
+			out.println("<html><head><script>window.location = \"" + link.URL + "\";</script></head><body></body></html>");
+
+		out.println();
+		out.flush();
+	}
 }
