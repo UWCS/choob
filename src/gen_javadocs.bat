da5170607d9fae2041e33b403b226946e38e8eab
tim <tim@retout.co.uk>
2007-08-28 18:08:52 +0000

    tim branch: Remove batch files.

--- src/gen_javadocs.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-REM Generates javadocs for the project and puts them in ../docs/javadocs
-
-javadoc -notimestamp -classpath .;lib/c3p0-0.9.0.2.jar;lib/msnm.jar;lib/jcfd.jar;lib/jazzy-core.jar;lib/bsh-2.0b4.jar;lib/mysql-connector-java-3.1.12-bin.jar;lib/pircbot.jar;lib/js-rhino-1.6r2.jar -d ../docs/javadocs/ uk.co.uwcs.choob uk.co.uwcs.choob.support uk.co.uwcs.choob.support.events uk.co.uwcs.choob.modules uk.co.uwcs.choob.plugins
