878f400ab416a7887b767eb0ae1a671fcf4d9a70
brad <brad@uwcs.co.uk>
2009-06-20 17:05:28 +0000

    Add remove and list commands to outputfilter. And add ordering to filters.
    Syntax:
    
      outputfilter.list <channel> [<id>]
      outputfilter.remove <channel> <id>

--- contrib/alpha/OutputFilter.java
+++ contrib/alpha/OutputFilter.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.List;
 
 import uk.co.uwcs.choob.modules.Modules;
@@ -8,19 +9,27 @@ import uk.co.uwcs.choob.support.IRCInterface;
 import uk.co.uwcs.choob.support.events.Message;
 
 
-class OutputFilterObject
+class OutputFilterObject implements Comparable<OutputFilterObject>
 {
-	public OutputFilterObject(final String target, final String expression, final String owner)
+	public OutputFilterObject(final String target, final String expression,
+		final String owner)
 	{
 		this.target = target;
 		this.converted = expression;
 		this.owner = owner;
 		this.id = 0;
 	}
+
 	public OutputFilterObject()
 	{
 		// Unhide
 	}
+
+	public int compareTo(OutputFilterObject other)
+	{
+		return Integer.valueOf(id).compareTo(Integer.valueOf(other.id));
+	}
+
 	public int id;
 	public String target;
 	public String converted;
@@ -45,10 +54,15 @@ public class OutputFilter
 		if (params.length < 3)
 			throw new ChoobBadSyntaxError();
 
-		mods.security.checkNickPerm(new ChoobPermission("plugin.outputfilter.add." + params[1]), mes);
+		mods.security.checkNickPerm(new ChoobPermission(
+			"plugin.outputfilter.add." + params[1]), mes);
 
-		final String output = doEval(params[2], "omg ponies", mes.getNick(), params[1]);
-		mods.odb.save(new OutputFilterObject(params[1], params[2], mes.getNick()));
+		final String output = doEval(params[2], "omg ponies",
+			mes.getNick(), params[1]);
+
+		mods.odb.save(new OutputFilterObject(params[1], params[2],
+			mes.getNick()));
+
 		irc.sendContextReply(mes, "Okay, sample output: " + output);
 	}
 
@@ -56,22 +70,102 @@ public class OutputFilter
 	{
 		final String[] params = mods.util.getParamArray(mes);
 
-		mods.security.checkNickPerm(new ChoobPermission("plugin.outputfilter.removeall." + params[1]), mes);
+		mods.security.checkNickPerm(new ChoobPermission(
+			"plugin.outputfilter.removeall." + params[1]), mes);
 
 		for (String channel : params)
-			for (OutputFilterObject o : mods.odb.retrieve(OutputFilterObject.class, whereTarget(channel)))
+		{
+			for (OutputFilterObject o : mods.odb.retrieve(
+				OutputFilterObject.class, whereTarget(channel)))
+			{
 				mods.odb.delete(o);
+			}
+		}
+
 		irc.sendContextReply(mes, "Okay!");
 	}
 
-	public String apiApply(final String target, final String nick, final String message)
+	public void commandRemove(Message mes)
+	{
+		final String[] params = mods.util.getParamArray(mes, 2);
+		if (params.length < 3)
+			throw new ChoobBadSyntaxError();
+
+		int item = Integer.parseInt(params[2]);
+
+		mods.security.checkNickPerm(new ChoobPermission(
+			"plugin.outputfilter.remove." + params[1]), mes);
+
+		final List<OutputFilterObject> filters = mods.odb.retrieve(
+			OutputFilterObject.class, whereTarget(params[1]));
+
+		Collections.sort(filters);
+
+		mods.odb.delete(filters.get(item));
+
+		irc.sendContextReply(mes, "Okay!");
+	}
+
+	public void commandList(Message mes)
+	{
+		final String[] params = mods.util.getParamArray(mes);
+		if (params.length < 2)
+			throw new ChoobBadSyntaxError();
+
+		String channel;
+		int item = -1;
+		boolean toolong = false;
+		int j = 0;
+
+		channel = params[1];
+		if (params.length > 2)
+			item = Integer.parseInt(params[2]);
+
+		irc.sendContextReply(mes, "OutputFilters for " + channel + ":");
+
+		final List<OutputFilterObject> filters = mods.odb.retrieve(
+			OutputFilterObject.class, whereTarget(channel));
+
+		Collections.sort(filters);
+
+		if(item != -1)
+		{
+			irc.sendContextReply(mes, "[" + item + "] -> " +
+				filters.get(item).converted);
+		}
+		else
+		{
+			for (OutputFilterObject o : filters)
+			{
+				if(!toolong && j > 3)
+				{
+					toolong = true;
+					j = 1;
+				}
+				else if(!toolong)
+				{
+					irc.sendContextReply(mes, "[" + j + "] -> " +
+						o.converted);
+				}
+				j++;
+			}
+			if(toolong)
+				irc.sendContextReply(mes, "    -> " + j + " more items..");
+		}
+	}
+
+	public String apiApply(final String target, final String nick,
+		final String message)
 	{
 		String working = message;
 		try
 		{
-			final List<OutputFilterObject> ret = mods.odb.retrieve(OutputFilterObject.class, whereTarget(target));
+			final List<OutputFilterObject> filters = mods.odb.retrieve(
+				OutputFilterObject.class, whereTarget(target));
 
-			for (OutputFilterObject a : ret)
+			Collections.sort(filters);
+
+			for (OutputFilterObject a : filters)
 			{
 				String converted = a.converted;
 				try
@@ -93,15 +187,18 @@ public class OutputFilter
 		}
 	}
 
-	private String doEval(String expression, String stdin, final String nick,
-			final String target) throws ChoobNoSuchCallException
+	private String doEval(String expression, String stdin,
+		final String nick, final String target)
+		throws ChoobNoSuchCallException
 	{
 		System.out.println("Applying " + expression + " to " + stdin);
-		return (String) mods.plugin.callAPI("Pipes", "Eval", expression, nick, target, stdin);
+		return (String)mods.plugin.callAPI("Pipes", "Eval", expression,
+			nick, target, stdin);
 	}
 
 	private String whereTarget(final String target)
 	{
-		return "WHERE target='" + mods.odb.escapeString(target) + "' ORDER BY RAND()";
+		return "WHERE target='" + mods.odb.escapeString(target) +
+			"' ORDER BY RAND()";
 	}
 }
