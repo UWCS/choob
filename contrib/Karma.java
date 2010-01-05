2625cdb676b1147ca6fab7cd4e6791aad66fdee4
brad <brad@uwcs.co.uk>
2010-01-05 01:06:22 +0000

    git rebase -i HEAD~~ -m ohshi-

--- contrib/Karma.java
+++ contrib/Karma.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
