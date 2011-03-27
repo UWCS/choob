00f09a375fff864ed3cf07c17057f10f693def0d
Chris West (Faux) <git@goeswhere.com>
2011-03-28 00:35:34 +0100

    What an Alias test would look like, but failing

---
+++ src/test/java/uk/co/uwcs/choob/AliasTest.java
@@ -0,0 +1,21 @@
+package uk.co.uwcs.choob;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.co.uwcs.choob.support.ChoobException;
+
+public class AliasTest extends AbstractPluginTest {
+
+	@Before
+	public void loadPlugin() throws ChoobException {
+		b.addPlugin("Talk");
+		b.addPlugin("Alias");
+	}
+
+	@Test
+	public void testSay() {
+		b.spinChannelMessage("~alias.alias say talk.say");
+		assertGetsResposne("#chan hi", "~say hi");
+	}
+}
