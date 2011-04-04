d8e4bf7bec581c0224e69ceccd3075226f57aa85
Chris West (Faux) <git@goeswhere.com>
2011-04-04 20:34:41 +0100

    SQLiteDialect autocleanups

--- src/main/java/dialect/SQLiteDialect.java
+++ src/main/java/dialect/SQLiteDialect.java
@@ -2,7 +2,7 @@ package dialect;
 /*
  * The author disclaims copyright to this source code. In place of
  * a legal notice, here is a blessing:
- * 
+ *
  * May you do good and not evil.
  * May you find forgiveness for yourself and forgive others.
  * May you share freely, never taking more than you give.
@@ -10,11 +10,11 @@ package dialect;
  */
 import java.sql.Types;
 
-import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.function.StandardSQLFunction;
-import org.hibernate.dialect.function.SQLFunctionTemplate;
-import org.hibernate.dialect.function.VarArgsSQLFunction;
 import org.hibernate.Hibernate;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.dialect.function.VarArgsSQLFunction;
 
 public class SQLiteDialect extends Dialect {
 	public SQLiteDialect() {
@@ -53,6 +53,7 @@ public class SQLiteDialect extends Dialect {
 				Hibernate.STRING));
 	}
 
+	@Override
 	public boolean supportsIdentityColumns() {
 		return true;
 	}
@@ -63,6 +64,7 @@ public class SQLiteDialect extends Dialect {
 	 }
 	 */
 
+	@Override
 	public boolean hasDataTypeInIdentityColumn() {
 		return false; // As specify in NHibernate dialect
 	}
@@ -76,77 +78,95 @@ public class SQLiteDialect extends Dialect {
 	 }
 	 */
 
+	@Override
 	public String getIdentityColumnString() {
 		// return "integer primary key autoincrement";
 		return "integer";
 	}
 
+	@Override
 	public String getIdentitySelectString() {
 		return "select last_insert_rowid()";
 	}
 
+	@Override
 	public boolean supportsLimit() {
 		return true;
 	}
 
+	@Override
 	public String getLimitString(String query, boolean hasOffset) {
 		return new StringBuffer(query.length() + 20).append(query).append(
 				hasOffset ? " limit ? offset ?" : " limit ?").toString();
 	}
 
+	@Override
 	public boolean supportsTemporaryTables() {
 		return true;
 	}
 
+	@Override
 	public String getCreateTemporaryTableString() {
 		return "create temporary table if not exists";
 	}
 
+	@Override
 	public boolean dropTemporaryTableAfterUse() {
 		return false;
 	}
 
+	@Override
 	public boolean supportsCurrentTimestampSelection() {
 		return true;
 	}
 
+	@Override
 	public boolean isCurrentTimestampSelectStringCallable() {
 		return false;
 	}
 
+	@Override
 	public String getCurrentTimestampSelectString() {
 		return "select current_timestamp";
 	}
 
+	@Override
 	public boolean supportsUnionAll() {
 		return true;
 	}
 
+	@Override
 	public boolean hasAlterTable() {
 		return false; // As specify in NHibernate dialect
 	}
 
+	@Override
 	public boolean dropConstraints() {
 		return false;
 	}
 
+	@Override
 	public String getAddColumnString() {
 		return "add column";
 	}
 
+	@Override
 	public String getForUpdateString() {
 		return "";
 	}
 
+	@Override
 	public boolean supportsOuterJoinForUpdate() {
 		return false;
 	}
 
+	@Override
 	public String getDropForeignKeyString() {
 		throw new UnsupportedOperationException(
 				"No drop foreign key syntax supported by SQLiteDialect");
 	}
 
+	@Override
 	public String getAddForeignKeyConstraintString(String constraintName,
 			String[] foreignKey, String referencedTable, String[] primaryKey,
 			boolean referencesPrimaryKey) {
@@ -154,15 +174,18 @@ public class SQLiteDialect extends Dialect {
 				"No add foreign key syntax supported by SQLiteDialect");
 	}
 
+	@Override
 	public String getAddPrimaryKeyConstraintString(String constraintName) {
 		throw new UnsupportedOperationException(
 				"No add primary key syntax supported by SQLiteDialect");
 	}
 
+	@Override
 	public boolean supportsIfExistsBeforeTableName() {
 		return true;
 	}
 
+	@Override
 	public boolean supportsCascadeDelete() {
 		return false;
 	}
