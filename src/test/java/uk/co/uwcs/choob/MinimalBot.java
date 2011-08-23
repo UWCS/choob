UWCS/choob

commit 142b7820c80988af69440a4e3cb3d3af9759f7dd

#390: getTrigger() was lying, breaking timedevents (utter failure)

src/main/java/uk/co/uwcs/choob/Bot.java
@@ -10,6 +10,7 @@ public interface Bot {
 	Modules getMods();
 	String getName();
 	String getNick();
+	String getTrigger();
 	String getTriggerRegex();
 
 	long getMessageDelay();

src/main/java/uk/co/uwcs/choob/Choob.java
@@ -349,6 +349,7 @@ final class Choob extends PircBot implements Bot
 	 * Return a string which can (and should) be prepended onto a line to
 	 * make it into a command.
 	 */
+	@Override
 	public String getTrigger()
 	{
 		return trigger;

src/main/java/uk/co/uwcs/choob/support/IRCInterface.java
@@ -253,7 +253,7 @@ public final class IRCInterface
 	 */
 	public String getTrigger()
 	{
-		return bot.getTriggerRegex();
+		return bot.getTrigger();
 	}
 
 	/**

src/test/java/uk/co/uwcs/choob/UnsupportedOperationBot.java
@@ -22,6 +22,11 @@ class UnsupportedOperationBot implements Bot {
 	}
 
 	@Override
+	public String getTrigger() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
 	public String getTriggerRegex() {
 		throw new UnsupportedOperationException();
 	}

commit abfb67402de425d3914bb34a64d6839acaa904d0

142b7820c80988af6944 broke tests

src/test/java/uk/co/uwcs/choob/MinimalBot.java
@@ -37,6 +37,11 @@ public final class MinimalBot extends UnsupportedOperationBot implements Closeab
 	}
 
 	@Override
+	public String getTrigger() {
+		return "~";
+	}
+
+	@Override
 	public String getTriggerRegex() {
 		return "~";
 	}