731f44bddd09ccd90b080fa8abe4bbedf5008b6a
Rayhaan Jaufeerally <rayhaan@uwcs.co.uk>
2015-07-18 14:11:34 +0100

    Refactor the ArgParse class for argument parsing, and add tests.

---
+++ src/main/java/uk/co/uwcs/choob/exceptions/UnmatchedBracketException.java
@@ -0,0 +1,9 @@
+package uk.co.uwcs.choob.exceptions;
+
+/**
+ * This exists to punish anyone who injects a dirty string with mismatched brackets.
+ */
+public class UnmatchedBracketException extends Exception {
+
+
+}
